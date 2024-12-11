198d8bc312ea1408d8323a6d48073254c73b98c3
Iant0 <dedikprianto6@gmail.com>
2024-12-11 20:38:03 +0700

    Belajar dengan javascript

---
+++ app.js
@@ -0,0 +1,40 @@
+// console.log('Sebelum pembanding');
+// if (1 + 1 === 3) {
+//   console.log('Perhitungannya masih betul ');
+// }
+
+// console.log('Setelah pembanding');
+
+// let angka = Math.random();
+// console.log(angka);
+
+// if (angka >= 0.5) {
+//   console.log('Angka lebih besar dari 0.5');
+// }
+// if (angka <= 0.5) {
+//   console.log('Angka lebih kecil dari 0.5');
+// }
+
+const iniHari = prompt('Isikan hari!').toLowerCase();
+
+if (iniHari === 'Senin') {
+  console.log('Selamat bekerja kawan!');
+} else if (iniHari === 'Sabtu') {
+  console.log('Semoga jangan cepat berlalu!');
+} else {
+  console.log('Yah biasa saja');
+}
+
+// const nilai = '99';
+
+// if (nilai < 50) {
+//   console.log('E');
+// } else if (nilai < 60) {
+//   console.log('D');
+// } else if (nilai < 70) {
+//   console.log('C');
+// } else if (nilai < 80) {
+//   console.log('B');
+// } else if (nilai < 100) {
+//   console.log('A');
+// }
